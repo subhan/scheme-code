09e1436fd61f9d1103e288cba9a2f433f06b9a9b
subhan <subhan.py@gmail.com>
2009-06-09 19:03:25 +0530

    Some documentation added to README

--- prime.el
+++ prime.el
@@ -4,14 +4,13 @@
 		((= n d) (display "it is a prime\n"))
 		(else (check-prime? n (+ d 1)))))
 
-
 (define (smallest-divisor n)
 	(find-divisor n 2))
 
 (define (find-divisor n test-divisor)
 	(cond ((> (square test-divisor) n) n)
 	      ((divides? test-divisor n) test-divisor)
-	      (else (find-divisor n (+ 1 test-divisor)))))	
+	      (else (find-divisor n (+ 1 test-divisor)))))
 	
 (define (divides? a b)
 	(= (remainder b a) 0))
@@ -25,6 +24,23 @@
 	(= n (smallest-divisor n)))
  
 
-(if (prime 7)
-	(display "it's a prime\n")
-	(display "it's not a prime\n")) 
+(define (next-prime n)
+	(if (prime n) 
+		n
+		(next-prime (+ n 1))))
+
+(define (lpd n p lp)
+	(let ((next (next-prime (+ 1 p))))
+	(cond ((= (remainder n p) 0) (lpd n next p))
+	      ((> (/ n 2) p) (lpd n next lp))
+	      (else lp))))
+
+(define fast-prime n)
+	(find-next-divisor
+			
+(define (larget-prime n)
+	(lpd n 2 1))
+
+;(if (prime 7)
+;	(display "it's a prime\n")
+;	(display "it's not a prime\n")) 
